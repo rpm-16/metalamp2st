34a74e6c9ea5a17535b31e09dc42ccdca4e4a598
RyzhkovPavel <rpm-16@mail.ru>
2025-03-22 20:46:20 +0300

    formelements + headers + pages

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import './sass/main.scss';
-const rpm = require('./js/rpm');
+
+// import './pug/blocks/input-dropdown/input-dropdown.js';
+import './pug/blocks/input-dropdown/input-dropdown_native.js';
+import './pug/blocks/range-slider/range-slider.js';
+import './pug/blocks/form-group/form-group.js';
+import './pug/blocks/calendar/calendar.js';
+
